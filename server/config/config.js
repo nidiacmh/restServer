ade9686fc7c1e53761afde2e79d24b6196271482
Caro <nidiacmh@gmail.com>
2019-02-24 13:16:17 -0600

    resServer -usuario-config

--- server/config/config.js
+++ server/config/config.js
@@ -14,14 +14,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Base de datos
 //====
 
-let urlDB;
-
-if (process.env.NODE_ENV) {
-  urlDB = 'mongodb://localhost:27017/cafe'
-
-}else{
+// let urlDB;
+//
+// if (process.env.NODE_ENV) {
+//   urlDB = 'mongodb://localhost:27017/cafe'
+//
+// }else{
   //Conexion a una base de datos en la nube:
   urlDB = 'mongodb+srv://nidia:Anilorac_aidin08@cafe-vj8ez.mongodb.net/test?retryWrites=true'
-}
+// }
 
 process.env.URLDB = urlDB;
