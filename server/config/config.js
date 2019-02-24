9c9bcd0f656d75c47abf00a2e52ace0532519fd3
Caro <nidiacmh@gmail.com>
2019-02-24 14:08:34 -0600

    primer commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
+require('dotenv').load();
 //===
 //Puerto
 //====
-
 process.env.PORT =process.env.PORT || 3000;
 
 //===
@@ -9,19 +9,19 @@ process.env.PORT =process.env.PORT || 3000;
 //====
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-
+const user = process.env.USR;
+const pwd = process.env.PASS;
 //===
 //Base de datos
 //====
 
-// let urlDB;
-//
-// if (process.env.NODE_ENV) {
-//   urlDB = 'mongodb://localhost:27017/cafe'
-//
-// }else{
+let urlDB;
+if (!process.env.NODE_ENV) {
+  urlDB = 'mongodb://localhost:27017/cafe'
+
+}else{
   //Conexion a una base de datos en la nube:
-  urlDB = 'mongodb+srv://nidia:Anilorac_aidin08@cafe-vj8ez.mongodb.net/test?retryWrites=true'
-// }
+  urlDB = 'mongodb+srv://'+user+':'+pwd+'@cafe-vj8ez.mongodb.net/test?retryWrites=true'
+}
 
 process.env.URLDB = urlDB;
