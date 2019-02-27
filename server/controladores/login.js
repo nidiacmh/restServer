90eea1a8e08bfd03d570b05af4b34f7de95c4965
Caro <nidiacmh@gmail.com>
2019-02-26 23:58:24 -0600

    Correccion de la carpeta public

--- server/controladores/login.js
+++ server/controladores/login.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {
-  OAuth2Client
-} = require('google-auth-library');
+const { OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID);
 const Usuario = require('../models/usuario');
 const app = express();
